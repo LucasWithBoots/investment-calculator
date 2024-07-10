9cf5a5b0f3586a908c9f6c1d5d05401e89cd33c9
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-07-10 18:53:43 -0300

    style: instalação do prettier e execução

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,15 +1,13 @@
-import {Component, Input} from '@angular/core';
-import {InvestmentResult} from "./investment.model";
-import {CurrencyPipe} from "@angular/common";
+import { Component, Input } from '@angular/core';
+import { InvestmentResult } from './investment.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [
-    CurrencyPipe
-  ],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.scss'
+  styleUrl: './investment-results.component.scss',
 })
 export class InvestmentResultsComponent {
   @Input() data!: InvestmentResult[];
--- src/app/investment-results/investment.model.ts
+++ src/app/investment-results/investment.model.ts
@@ -1,15 +1,15 @@
 export interface Investment {
-  initialInvestment: number
-  annualInvestment: number
-  expectedReturn: number
-  duration: number
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
 }
 
 export interface InvestmentResult {
-  annualInvestment: number
-  interest: number
-  totalAmountInvested: number
-  totalInterest: number
-  valueEndOfYear: number
-  year: number
+  annualInvestment: number;
+  interest: number;
+  totalAmountInvested: number;
+  totalInterest: number;
+  valueEndOfYear: number;
+  year: number;
 }
