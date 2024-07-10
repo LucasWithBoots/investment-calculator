62b20aae3f95cbef40c33c46bb68d8007b07cd98
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-07-09 23:29:14 -0300

    feat: formulario salvando dados

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,18 +1,24 @@
 import {Component} from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.scss'
 })
 export class UserInputComponent {
+  enteredInitialInvestment!: number;
+  enteredAnnualInvestment!: number;
+  enteredExpectedReturn!: number;
+  enteredDuration!: number;
+
 
   onSubmit() {
-
+    console.log(this.enteredInitialInvestment)
   }
 }
