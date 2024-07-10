8523e492e1a10045022b6103438ab4c25486f833
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-07-09 23:09:37 -0300

    feat: estilização

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.scss'
 })
 export class UserInputComponent {
 
+  onSubmit() {
+
+  }
 }
